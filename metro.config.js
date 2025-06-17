697bf12bd54ffcad7f8f4200927b5c720bef179b
Kirill <hi.sender@gmail.com>
2025-06-17 20:30:17 +0200

    lint

--- metro.config.js
+++ metro.config.js
@@ -1,6 +1,8 @@
-const { getDefaultConfig } = require("expo/metro-config");
-const { withNativeWind } = require('nativewind/metro');
+const { getDefaultConfig } = require('expo/metro-config');
+const path = require('path');
 
 const config = getDefaultConfig(__dirname);
+
+const { withNativeWind } = require('nativewind/metro');
 
 module.exports = withNativeWind(config, { input: './global.css' });
